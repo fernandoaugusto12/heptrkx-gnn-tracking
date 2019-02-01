6426e1920bef7cbc24a7bf21a8d783e408b85692
Saliya Ekanayake <esaliya@gmail.com>
2019-01-31 16:02:19 -0800

    adding profile output to master

--- scripts/prof_trainSegmentClassifier_interactive.sh
+++ scripts/prof_trainSegmentClassifier_interactive.sh
@@ -5,8 +5,13 @@ mkdir -p logs
 
 export OMP_NUM_THREADS=32
 #medium dataset
-#srun -n 1 python -m cProfile -o ${SLURMD_NODENAME}.prof.med.out.txt ./train.py -v -d configs/segclf_med_ep1.yaml 2>&1|tee ${SLURMD_NODENAME}.med.out.txt
-ep=16
-srun -n 1 python -m cProfile -o ${SLURMD_NODENAME}.prof.ep${ep}.med.out.txt ./train.py -v -d configs/segclf_med_ep${ep}.yaml 2>&1|tee ${SLURMD_NODENAME}.med.${ep}.out.txt
+srun -n 1 python -m cProfile -o ${SLURMD_NODENAME}.prof.med.out.txt ./train.py -v -d configs/segclf_med_ep1.yaml 2>&1|tee ${SLURMD_NODENAME}.med.out.txt
+
+#vtune
+#srun -n 1 amplxe-cl -collect hotspots -r vtune-profs -trace-mpi python -m cProfile -o ${SLURMD_NODENAME}.prof.ep1.med.out.txt ./train.py -v -d configs/segclf_med_ep1.yaml 2>&1|tee ${SLURMD_NODENAME}.med.out.txt
+
+#ep=16
+#srun -n 1 python -m cProfile -o ${SLURMD_NODENAME}.prof.ep${ep}.med.out.txt ./train.py -v -d configs/segclf_med_ep${ep}.yaml 2>&1|tee ${SLURMD_NODENAME}.med.${ep}.out.txt
+
 #small dataset
 #srun -n 1 python ./train.py -v -d configs/segclf_small.yaml 2>&1|tee small.out.txt
