1dd00f87e53b0b73b473dd964df85cc5e354bd66
Sitong An <ansitong@gmail.com>
2019-06-12 16:28:21 +0200

    Add test plot and visualisation

--- datasets/graph.py
+++ datasets/graph.py
@@ -57,3 +57,58 @@ def load_graph(filename):
 
 def load_graphs(filenames, graph_type=Graph):
     return [load_graph(f, graph_type) for f in filenames]
+
+
+#thanks Steve :-)
+def draw_sample(X, Ri, Ro, y, out,
+                cmap='bwr_r', 
+                skip_false_edges=True,
+                alpha_labels=False, 
+                sim_list=None): 
+    # Select the i/o node features for each segment    
+    feats_o = X[Ro]
+    feats_i = X[Ri]    
+    # Prepare the figure
+    fig, (ax0,ax1) = plt.subplots(1, 2, figsize=(20,12))
+    cmap = plt.get_cmap(cmap)
+    
+    if sim_list is None:    
+        # Draw the hits (layer, x, y)
+        ax0.scatter(X[:,0], X[:,2], c='k')
+        ax1.scatter(X[:,1], X[:,2], c='k')
+    else:        
+        ax0.scatter(X[:,0], X[:,2], c='k')
+        ax1.scatter(X[:,1], X[:,2], c='k')
+        ax0.scatter(X[sim_list,0], X[sim_list,2], c='b')
+        ax1.scatter(X[sim_list,1], X[sim_list,2], c='b')
+    
+    # Draw the segments
+    for j in range(y.shape[0]):
+        if not y[j] and skip_false_edges: continue
+        if alpha_labels:
+            seg_args = dict(c='k', alpha=float(y[j]))
+        else:
+            seg_args = dict(c=cmap(float(y[j])))
+        ax0.plot([feats_o[j,0], feats_i[j,0]],
+                 [feats_o[j,2], feats_i[j,2]], '-', **seg_args)
+        ax1.plot([feats_o[j,1], feats_i[j,1]],
+                 [feats_o[j,2], feats_i[j,2]], '-', **seg_args)
+        
+     
+    if out is not None:
+        for j in range(out.shape[0]):
+            if out[j]<0.5 : continue
+            seg_args = dict(c='r')        
+            ax0.plot([feats_o[j,0], feats_i[j,0]],
+                     [feats_o[j,2], feats_i[j,2]], '-', **seg_args)
+            ax1.plot([feats_o[j,1], feats_i[j,1]],
+                     [feats_o[j,2], feats_i[j,2]], '-', **seg_args)
+        
+        
+    # Adjust axes
+    ax0.set_xlabel('$x$ [cm]')
+    ax1.set_xlabel('$y$ [cm]')
+    ax0.set_ylabel('$layer$ [arb]')
+    ax1.set_ylabel('$layer$ [arb]')
+    plt.tight_layout()
+    return fig;
